ccef9250c61b7d193a078cabadac86241be992c2
Soo Hwan Park <suwanny@gmail.com>
2013-04-30 03:40:23 -0700

    change..

--- server/nodejs/http.js
+++ server/nodejs/http.js
@@ -1,13 +1,14 @@
 var http = require('http');
 
+var counter = 0;
 http.createServer(function (req, res) {
-  console.log("get a request.. from " + 
-    req.connection.remoteAddress + ":" +
-    req.connection.remotePort);
-  
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  console.log("sent a response");
+  counter += 1;
+  console.log("[" + counter + "] get a request.. from " + 
+    req.connection.remoteAddress + ":" + req.connection.remotePort);
+
+  res.writeHead(200, {'Content-Type': 'text/plain', 'Server':'NodeJS'});
+  res.end("hello, world!");
+  // console.log("sent a response");
 }).listen(5000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:5000/');
